a5d3f79cea30bd664645611edd480437b60c8909
Clinton.Botha <clinton.botha@coveainsurance.co.uk>
2022-02-11 08:53:40 +0200

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import logo from "./flywheel.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <svg
+          width="63.751999mm"
+          height="99.307999mm"
+          viewBox="0 0 63.751999 99.307999"
+          version="1.1"
+          id="svg21708"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <defs id="defs21705" />
+          <g id="layer1">
+            <g id="g21731" transform="translate(-0.75673336,0.08531402)">
+              <path
+                fill="#176897"
+                fillOpacity="1"
+                fillRule="nonzero"
+                stroke="none"
+                d="M 0.75673336,-0.08531248 C 24.064873,-0.08941248 46.633,8.1043675 64.508918,23.062971 L 0.77740536,99.223006 Z m 0,0"
+                id="path7064"
+              />
+              <path
+                fill="#737373"
+                fillOpacity="1"
+                fillRule="nonzero"
+                stroke="none"
+                d="M 0.75811136,4.9114525 C 22.893536,4.9073525 44.326164,12.6891 61.302217,26.895299 L 0.77740536,99.223006 Z m 0,0"
+                id="path7082"
+              />
+            </g>
+          </g>
+        </svg>
+      </header>
+    </div>
+  );
+}
+
+export default App;
